bb80ef6e9c5e4bae2d41a7bb758352eca7c5d8d7
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-03-30 13:08:29 -0400

    a

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import * as Phaser from './lib/phaser.js'
 
-export * as default new from Phaser.game({
+export * as default from new Phaser.game({
   type: Phaser.AUTO,
   width: 480,
   height: 640
